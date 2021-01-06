4d93eb7758e9e864eed63e20ff66d8c1a8481de6
Guilherme Mallmann <guilherme.costa@firedev.com.br>
2021-01-06 16:07:43 -0300

    Criando a estrutura inicial e configurações do client

--- src/Client.php
+++ src/Client.php
@@ -6,5 +6,51 @@ namespace Firedev\Pix\Gerencianet;
 
 class Client
 {
+    /**
+     * @var string
+     */
+    private $caminhoArquivoCertificado;
 
+    /**
+     * @var string
+     */
+    private $clientId;
+
+    /**
+     * @var string
+     */
+    private $clientSecret;
+
+    /**
+     * @var array
+     */
+    private $infoAmbientes;
+
+    private $httpClient;
+
+
+
+    /**
+     * Client constructor.
+     * @param $caminhoArquivoCertificado
+     * @param $clientId
+     * @param $clientSecret
+     * @param $httpClient
+     */
+    public function __construct($caminhoArquivoCertificado, $clientId, $clientSecret)
+    {
+        $this->caminhoArquivoCertificado  = $caminhoArquivoCertificado;
+        $this->clientId                   = $clientId;
+        $this->clientSecret               = $clientSecret;
+        $this->inicializar();
+    }
+
+    private function inicializar()
+    {
+        $this->infoAmbientes = json_decode(file_get_contents(__DIR__ . '/info-ambientes.json'), true);
+        $this->httpClient    = new \GuzzleHttp\Client([
+            'base_uri' => $this->infoAmbientes['homologacao']['pix_base_uri'],
+            'timeout'  => 5
+        ]);
+    }
 }
