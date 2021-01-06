b5a051e49a74e0fdfd68eafc8fd88c0b136be48c
Guilherme Mallmann <guilherme.costa@firedev.com.br>
2021-01-06 16:32:28 -0300

    Criando a funcionalidade de setar o client como modo de produção

--- src/Client.php
+++ src/Client.php
@@ -6,6 +6,10 @@ namespace Firedev\Pix\Gerencianet;
 
 class Client
 {
+    const MODO_PRODUCAO    = 'producao';
+
+    const MODO_HOMOLOGACAO = 'homologacao';
+
     /**
      * @var string
      */
@@ -26,9 +30,15 @@ class Client
      */
     private $infoAmbientes;
 
+    /**
+     * @var \Psr\Http\Client\ClientInterface
+     */
     private $httpClient;
 
-
+    /**
+     * @var string
+     */
+    private $modo;
 
     /**
      * Client constructor.
@@ -45,12 +55,35 @@ class Client
         $this->inicializar();
     }
 
-    private function inicializar()
+    /**
+     * Reescreve o client http com a url base da API de produção da gerencianet e seta o modo como produção.
+     */
+    public function ativaModoProducao(): void
+    {
+        $this->httpClient = new \GuzzleHttp\Client([
+            'base_uri' => $this->infoAmbientes['producao']['pix_url_base'],
+            'timeout'  => 5
+        ]);
+        $this->modo = self::MODO_PRODUCAO;
+    }
+
+    /**
+     * Lê o arquivo de configuração e inicializa o componente de comunicação http.
+     * Por padrão o client é instanciado em modo de desenvolvimento,
+     * se comunicando com a API de homologação da Gerencianet.
+     */
+    private function inicializar(): void
     {
         $this->infoAmbientes = json_decode(file_get_contents(__DIR__ . '/info-ambientes.json'), true);
         $this->httpClient    = new \GuzzleHttp\Client([
-            'base_uri' => $this->infoAmbientes['homologacao']['pix_base_uri'],
+            'base_uri' => $this->infoAmbientes['homologacao']['pix_url_base'],
             'timeout'  => 5
         ]);
+        $this->modo = self::MODO_HOMOLOGACAO;
+    }
+
+    public function estaEmModoProducao(): bool
+    {
+        return $this->modo === self::MODO_PRODUCAO;
     }
 }
